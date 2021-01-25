3de6c1939a5223fee538197619d71550e9029086
garvmathur7700 <garvmathur7700@gmail.com>
2021-01-26 01:13:49 +0530

    first commit

---
+++ new2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    int c;
+    printf("Enter any two numbers: \n");
+    scanf("%d %d", &a, &b);
+    c = a + b;
+    printf("%d + %d = %d", a, b, c);
+    return 0;
+}
